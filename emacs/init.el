5db5fdc53ebeae849954906ae3edaf7772d652b2
Ronnie Holm <mail@bugfree.dk>
2013-11-06 20:40:43 +0100

    Remove package

--- emacs/init.el
+++ emacs/init.el
@@ -81,8 +81,7 @@
 
 ;;; Install fsharp-mode
 (unless (package-installed-p 'fsharp-mode)
-  (package-install 'fsharp-mode
-		   'color-theme))
+  (package-install 'fsharp-mode))
 
 (require 'fsharp-mode)
 
