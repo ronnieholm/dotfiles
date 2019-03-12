c197067f8dd82003de08201376ce5ad9bb50eab3
Ronnie Holm <mail@bugfree.dk>
2019-03-13 00:21:10 +0100

    Updated

--- init.el
+++ init.el
@@ -173,16 +173,3 @@
   (setq-default indent-tabs-mode nil))
 
 (load-theme 'deeper-blue)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(omnisharp which-key use-package try spaceline ox-twbs markdown-mode magit git-gutter dashboard company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
