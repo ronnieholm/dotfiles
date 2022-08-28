05b7d23a510c3946f560182486fee68b009858e3
Ronnie Holm <mail@bugfree.dk>
2022-08-28 22:23:36 +0200

    update

--- emacs/init.el
+++ emacs/init.el
@@ -51,6 +51,7 @@
 
 ;; dired
 (setq dired-listing-switches "-alh")
+(setf dired-kill-when-opening-new-dired-buffer t)
 
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
@@ -246,3 +247,17 @@
 (sunrise-sunset)
 
 (use-package helm-rg)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fe1c13d75398b1c8fd7fdd1241a55c286b86c3e4ce513c4292d01383de152cb7" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
