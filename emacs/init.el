40f1c18ea1d14874066c842ec549919969edd723
Ronnie Holm <mail@bugfree.dk>
2022-09-04 16:54:52 +0200

    Add lsp-mode

--- emacs/init.el
+++ emacs/init.el
@@ -195,6 +195,24 @@
              (local-set-key (kbd "C-c c") 'projectile-compile-project)
              (setq truncate-lines -1)))
 
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+         (csharp-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+(require 'dap-netcore)
+
 (use-package paredit)
 
 (add-hook 'emacs-lisp-mode-hook
