98ee9ba5469b9a9a4f07f3e8cb07d8b4fb3bf2e7
Ronnie Holm <mail@bugfree.dk>
2022-12-18 15:08:44 +0100

    Update

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+-- Inspired by ThePrimeagen 0 to LSP : Neovim RC From Scratch (https://www.youtube.com/watch?v=w7i4amO_zaE)
+-- https://github.com/ThePrimeagen/init.lua/blob/master/after/plugin/lsp.lua
+
+require("rh")
+
---
+++ nvim/lua/rh/init.lua
@@ -0,0 +1,2 @@
+require("rh.set")
+require("rh.remap")
